14a0c3330afef996262077e684118543f5b85434
wake3532 <70120646+wake3532@users.noreply.github.com>
2021-01-26 21:20:11 +0900

    Update miso.py

--- miso.py
+++ miso.py
@@ -22,20 +22,6 @@ async def on_ready():
     print('아이디 : {}'.format(client.user.id))
 
 @client.event
-async def on_ready():
-    print('봇이 로그인 하였습니다.')
-    print(' ')
-    print('닉네임 : {}'.format(client.user.name))
-    print('아이디 : {}'.format(client.user.id))
-    while True:
-        user = len(client.users)
-        server = len(client.guilds)
-        messages = [" 봇 점검자 : 🌸 LYZE 🌸#2021  ", "봇 에러 발생시 🌸 LYZE 🌸#2021한테 디엠 부탁드려요" , " 봇 호스팅 상태 : ㄱㅊ "]
-        for (m) in range(5):
-            await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(name=messages[(m)], type=discord.ActivityType.watching))
-            await asyncio.sleep(4)
-
-@client.event
 async def on_member_join(member):
     embed=discord.Embed(title="<:4114110118_pandaclipartemojipand:798106315716952074> 익명님이 입장하셧습니다. 환영해요!")
     embed.set_image(url="https://media.discordapp.net/attachments/760040615781072946/803580701819142184/banner.gif")
